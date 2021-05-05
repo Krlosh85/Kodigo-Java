0c84f3e7face4166a0e08d9dade8391e450861d3
Krlosh85 <carlitos_herrera_jr@hotmail.com>
2021-05-05 15:48:49 -0600

    Fix script of Calorias App

--- 2.CaloriasApp/script.js
+++ 2.CaloriasApp/script.js
@@ -1,2 +1,8 @@
 //Cada vez que se le de click a un botón este cambiará
 //Su estilo a como que esta "seleccionado", incluyendo su valor
+var manzana = document.getElementById("manzana").addEventListener("click", selectedButton(), false);
+
+function selectedButton(selected){
+    this.selected = selected;
+    document.getElementById(selected).classList.add("img-food-selected");
+}
